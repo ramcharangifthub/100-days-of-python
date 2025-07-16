f7c78faafcb08dec8b137a9b08e2327637fccbc9
Ram Charan <rcyerramsetti@gmail.com>
2025-07-16 11:38:04 +0530

    Brand Name generator

--- DAY 1 .py
+++ DAY 1 .py
@@ -1,4 +1,4 @@
 print("welcome to Brand Name generator")
 city = input("What is the city you grew up in?")
 pet = input("What is your pet name?")
-print(f"your Brand name is {city} {pet}")
+print("your Brand name is" + city + pet)
